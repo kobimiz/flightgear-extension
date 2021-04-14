740551dae9d643ce3923f616413456c5fabae788
tomtr <zliltom@gmail.com>
2021-04-14 18:59:53 +0300

    user story 3 (not finished)

---
+++ viewModels/JoystickViewModel.cs
@@ -0,0 +1,63 @@
+﻿using flightgearExtension.classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace flightgearExtension.viewModels
+{
+    public class JoystickViewModel : ViewModel
+    {
+        public JoystickViewModel(Model model) : base(model)
+        {
+
+        }
+        public override void setModel(Model m)
+        {
+            base.setModel(m);
+            if (m == null)
+                return;
+            model.PropertyChanged += delegate (object sender, PropertyChangedEventArgs e)
+            {
+                if (e.PropertyName == "frameIndex")
+                {
+                    NotifyPropertyChanged("rudderLabel");
+                    NotifyPropertyChanged("aileronLabel");
+                    NotifyPropertyChanged("throttleLabel");
+                    NotifyPropertyChanged("elevatorLabel");
+                }
+            };
+
+        }
+        public string rudderLabel
+        {
+            get => getDataFromCurrFrameByName("rudder");
+        }
+        public string aileronLabel
+        {
+            get => getDataFromCurrFrameByName("aileron");
+        }
+        public string throttleLabel
+        {
+            get => getDataFromCurrFrameByName("throttle");
+        }
+        public string elevatorLabel
+        {
+            get => getDataFromCurrFrameByName("elevator");
+        }
+        private string getDataFromCurrFrameByName(string name)
+        {
+            if (model.Data == null || model.headings == null)
+                return null;
+            int index = Array.FindIndex(model.headings, (value) => value == name);
+            if (index == -1)
+                return null;
+            if (model.frameIndex >= model.Data.Length)
+                return model.Data[model.Data.Length - 1].Split(",")[index];
+            return model.Data[model.frameIndex].Split(",")[index];
+        }
+    }
+}
---
+++ views/JoystickView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.IO;
+using System.Net.Sockets;
+
+namespace flightgearExtension.viewModels
+{
+    public partial class JoystickView : UserControl
+    {
+        public viewModels.JoystickViewModel vm;
+        public JoystickView()
+        {
+            InitializeComponent();
+            vm = new JoystickViewModel(null);
+            DataContext = vm;
+        }
+        /*public void setValues()
+        {
+            // load variable values from xml file
+            //string variableValues = classes.Utility.getVariableValue(classes.Utility.getProtocolDir() + "\\playback_small.xml","rudder");
+            //rudderSlider.Value=(float.Parse(variableValues[2]));
+        }*/
+    }
+}
